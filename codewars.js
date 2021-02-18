4320418a2a7d1ce2fb2e30098a5d0d074fa7dd5a
Jack <jacksonmorgan@hey.com>
2021-02-17 21:59:41 -0500

    todays

--- codewars.js
+++ codewars.js
@@ -105,4 +105,23 @@ const map = (array) => {
 }
 
 map([1,2,3])
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+// Return True if you're better, else False!
+const betterThanAverage = (array, myScore) => {
+  let scoresTotal = 0
+  for (let score of array) {
+    scoresTotal += score 
+  }
+  let scoresAverage = scoresTotal / array.length
+
+  if (scoresAverage > myScore) {
+    return false
+  } else {
+    return true
+  }
+}
+
+betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)
